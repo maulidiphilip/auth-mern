684840d452c653ffc649a8f9ebccd3f0230dfec8
MAULIDI <maulidiphilip@gmail.com>
2025-01-15 12:39:43 +0200

    The form for reseeting password created

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,7 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { useState } from "react";
+
 const ResetPassword = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
   return (
-    <div>
-      <h1>Reset password page</h1>
+    <div className="flex items-center justify-center min-h-screen  bg-gradient-to-br from-blue-200 to-purple-400">
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        alt="Logo"
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+      />
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Reset Password
+        </h1>
+        <p className="text-center mb-6 text-indigo-300 ">Enter your email.</p>
+        <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+          <img src={assets.mail_icon} alt="" className="w-3 h-3" />
+          <input
+            type="email"
+            placeholder="Email"
+            className="bg-transparent outline-none text-white"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <button className="w-full py-2.5 mt-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
