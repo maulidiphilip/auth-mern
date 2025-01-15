134815e012012ff1ffa90d1b928f01696c942c64
MAULIDI <maulidiphilip@gmail.com>
2025-01-15 12:24:25 +0200

    The account verification on the frontend with OTP verifivation is a success

--- client/src/pages/VerifyEmail.jsx
+++ client/src/pages/VerifyEmail.jsx
@@ -1,5 +1,123 @@
+import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import React, { useContext, useEffect } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { AppContext } from "../store/AppContext";
+
 const VerifyEmail = () => {
-  return <div>VerifyEmail</div>;
+  const navigate = useNavigate();
+  const inputRefs = React.useRef([]);
+
+  axios.defaults.withCredentials = true;
+
+  const { backendURL, isLoggedIn, setIsLoggedIn, userData, getUserData } =
+    useContext(AppContext);
+
+  console.log(userData, "userdata");
+
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      // Move focus to the next input field
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRefs.current[index - 1].focus(); // Focus on the previous input field
+    }
+  };
+  // what about a paste feature?
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const pastedData = e.clipboardData.getData("text").trim(); // Get the pasted string
+
+    if (pastedData.length === inputRefs.current.length) {
+      pastedData.split("").forEach((char, index) => {
+        if (inputRefs.current[index]) {
+          inputRefs.current[index].value = char; // Set value for each input
+        }
+      });
+
+      // Automatically focus the last field
+      inputRefs.current[inputRefs.current.length - 1].focus();
+    } else {
+      // Optional: Display a toast or error for invalid paste length
+      toast.error("Invalid code length");
+    }
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      // getthe OTP
+      const otpArray = inputRefs.current.map((e) => e.value);
+      const otp = otpArray.join("");
+
+      // make an api call
+      const { data } = await axios.post(
+        backendURL + "/api/auth/verify-account",
+        { otp }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        getUserData();
+        navigate("/");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Something went wrong.");
+    }
+  };
+
+  useEffect(() => {
+    isLoggedIn && userData && userData.isVerified && navigate("/");
+  }, [isLoggedIn, userData]);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen  bg-gradient-to-br from-blue-200 to-purple-400">
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        alt="Logo"
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+      />
+      <form
+        onSubmit={onSubmitHandler}
+        className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm"
+      >
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Email Verify OTP
+        </h1>
+        <p className="text-center mb-6 text-indigo-300 ">
+          Enter 6-digit code sent to your email.
+        </p>
+        <div className="flex justify-between mb-8" onPaste={handlePaste}>
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <input
+                type="text"
+                maxLength="1"
+                key={index}
+                required
+                className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
+                ref={(e) => (inputRefs.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+              />
+            ))}
+        </div>
+        <button className="w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full">
+          Verify Email
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default VerifyEmail;
