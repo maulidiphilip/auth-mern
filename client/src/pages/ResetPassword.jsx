ae6066e4e523570b1f441009ba0e9f33d61550c7
MAULIDI <maulidiphilip@gmail.com>
2025-01-16 09:16:55 +0200

    the reset password forms UI created

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,11 +1,48 @@
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const ResetPassword = () => {
   const navigate = useNavigate();
+  const inputRefs = React.useRef([]);
+  axios.defaults.withCredentials = true;
 
   const [email, setEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      // Move focus to the next input field
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRefs.current[index - 1].focus(); // Focus on the previous input field
+    }
+  };
+  // what about a paste feature?
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const pastedData = e.clipboardData.getData("text").trim(); // Get the pasted string
+
+    if (pastedData.length === inputRefs.current.length) {
+      pastedData.split("").forEach((char, index) => {
+        if (inputRefs.current[index]) {
+          inputRefs.current[index].value = char; // Set value for each input
+        }
+      });
+
+      // Automatically focus the last field
+      inputRefs.current[inputRefs.current.length - 1].focus();
+    } else {
+      // Optional: Display a toast or error for invalid paste length
+      toast.error("Invalid code length");
+    }
+  };
   return (
     <div className="flex items-center justify-center min-h-screen  bg-gradient-to-br from-blue-200 to-purple-400">
       <img
@@ -32,6 +69,59 @@ const ResetPassword = () => {
         </div>
         <button className="w-full py-2.5 mt-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full">
           Submit
+        </button>
+      </form>
+
+      {/* a new form for adding otp */}
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Reset Password OTP
+        </h1>
+        <p className="text-center mb-6 text-indigo-300 ">
+          Enter 6-digit code sent to your email.
+        </p>
+        <div className="flex justify-between mb-8" onPaste={handlePaste}>
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <input
+                type="text"
+                maxLength="1"
+                key={index}
+                required
+                className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
+                ref={(e) => (inputRefs.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+              />
+            ))}
+        </div>
+        <button className="w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full">
+          Submit
+        </button>
+      </form>
+
+      {/* enter new password */}
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          New Password
+        </h1>
+        <p className="text-center mb-6 text-indigo-300 ">
+          Enter your new Password below.
+        </p>
+        <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+          <img src={assets.lock_icon} alt="" className="w-3 h-3" />
+          <input
+            type="password"
+            placeholder="New Password"
+            className="bg-transparent outline-none text-white"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button className="w-full py-2.5 mt-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full">
+          Save
         </button>
       </form>
     </div>
